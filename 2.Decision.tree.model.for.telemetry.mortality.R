ebe0dd975f9b27a66a8a48ed4410f1e565db875a
Evan <ebuechley@gmail.com>
2019-07-23 12:04:25 -0600

    worked on decision tree model of vulture fate (not complete!)

--- 2.Decision.tree.model.for.telemetry.mortality.R
+++ 2.Decision.tree.model.for.telemetry.mortality.R
@@ -1,4 +1,5 @@
 # decision tree model to predict mortality from telemetry data
+library(rpart)
 
 #Set WD
 setwd("~/Documents/GitHub/EV - TV Survival Study")
@@ -7,25 +8,14 @@ setwd("~/Documents/GitHub/EV - TV Survival Study")
 rm(list = ls())
 
 #read in data
-d = read.csv("ev.tv.filtered.csv", colClasses = "character")
-summ = read.csv("ev.tv.summary.merged.csv", colClasses = "character")
+#d = read.csv("ev.tv.filtered.csv")
 summary(d)
-summary(summ$fate)
+summ = read.csv("ev.tv.summary.merged.csv")
+summary(summ)
 
-#need to create a data table that summarizes variables for the last 10 days...
-#of transmission for each id (per Sergio et al. 2019)
+#need to create a data table that summarizes status or change in variables ...
+#for the last 10 days of transmission for each id (per Sergio et al. 2019)
 
-
-#add column of fate to full dataset
-# replace cell values for matching ids
-d$fate = NA
-
-for (i in unique(summ$id.tag)) { 
-  d$fate[which(d$id.tag == i)] = summ$fate[which(summ$id.tag == i)]
-}
-
-d$fate = as.factor(d$fate)
-summary(d$fate)
 
 #build decision trees to predict fate of tracked individuals
 #Potential Variables (per Sergio et al. 2019): 
@@ -39,7 +29,19 @@ summary(d$fate)
 #GPS spatial error --- low spatial error may indicate lack of movement, and thus mortality (??? not sure I buy this)
 #Argos location classes 
 
+#d.tree = rpart(fate ~ dist + dt.days + NSD, data = d, method = "class")
 
-library(rpart)
-names(d)
-d.tree = rpart(fate ~ dist + dt.days + NSD, data = d, method = "class")
+
+###############################################
+#add column of fate to full dataset
+# replace cell values for matching ids
+d$fate = NA
+
+for (i in unique(summ$id.tag)) { 
+  d$fate[which(d$id.tag == i)] = summ$fate[which(summ$id.tag == i)]
+}
+
+d$fate = as.factor(d$fate)
+summary(d$fate)
+
+
