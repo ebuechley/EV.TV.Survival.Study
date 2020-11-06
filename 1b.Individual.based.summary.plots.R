beab593085df3a9d6297e3bd8eb33a5ddb5a58e7
evan.buechley <ebuechley@gmail.com>
2020-11-06 10:39:23 -0700

    Rev1 updates

--- 1b.Individual.based.summary.plots.R
+++ 1b.Individual.based.summary.plots.R
@@ -9,22 +9,17 @@ setwd("~/Google Drive/Research Projects/EV-TV Survival Study/Dataset/Final/Rev1/
 
 #read data
 d = read.csv("./Data/ev.filtered.csv")
-summary(d)
-names(d)
-head(d$sensor.type)
-summary(d$tag)
-head(d$timestamp)
 d$timestamp = ymd_hms(d$timestamp)
 summary(d$ND)
-unique(d$id)
+unique(d$id) #231
 names(d)
 
 #plots for each id
 # create for loop to produce ggplot2 graphs 
 library(gridExtra)
 names(d)
-d$id.yr <- c(paste(d$id,d$year,sep="_")) 
-unique(d$id.yr)
+d$id.tag.year <- c(paste(d$id.tag,d$year,sep="_")) 
+unique(d$id.tag.year)
 d$timestamp = as.Date(d$timestamp)
 summary(d$timestamp)
 
@@ -66,35 +61,36 @@ summary(mig.stage.new)
 #clean up 
 names(mig.stage.new)
 mig.stage.new = mig.stage.new[,c("id.tag.year", "X1.x", "X2.x", "X3.x", "X4.x", "X5.x", "X6.x", 
-                                 "X7.x", "X8.x", "X9.x", "X10.x", "X11.x", "X12.x")]
-names(mig.stage.new) <- c("id.tag.year","X1","X2","X3","X4","X5","X6","X7","X8","X9","X10","X11","X12")
+                                 "X7.x", "X8.x", "X9.x", "X10.x", "X11.x", "X12.x","coments.1","coments.2","coments.3")]
+names(mig.stage.new) <- c("id.tag.year","X1","X2","X3","X4","X5","X6","X7","X8","X9","X10","X11","X12","comments.1","comments.2","comments.3")
 summary(mig.stage.new)
 write.csv(mig.stage.new,"./Data/Mig_stage_matrix.Rev1.csv", row.names=FALSE)
 
 #export individual id.yr summary plots
-for (i in unique(d$id.yr)) { 
+names(d)
+for (i in unique(d$id.tag.year)) { 
   #net displacement
   plot1 <- 
-    ggplot(aes(timestamp, ND), data = subset(d, id.yr == i))  + 
+    ggplot(aes(timestamp, ND), data = subset(d, id.tag.year == i))  + 
     theme_bw() + geom_line() + labs(x = "date", y = "net displacement (degrees)") + 
     scale_x_date(date_breaks = "1 month", date_labels = "%b", date_minor_breaks = "1 month") 
   
   #fix rate
   plot2 <- 
-    ggplot(aes(timestamp, dt.days), data = subset(d, id.yr == i))  + 
+    ggplot(aes(timestamp, dt.days), data = subset(d, id.tag.year == i))  + 
     theme_bw() + geom_line() + labs(x = "date", y = "time between fixes (days)") + 
     scale_x_date(date_breaks = "1 month", date_labels = "%b", date_minor_breaks = "1 month")
   
   #GPS tracks
   plot3 <- 
     ggplot() + annotation_map(map_data("world"), fill = 'grey', color = "white")  + coord_quickmap() + theme_bw() +
-    geom_path(data = subset(d, id.yr ==  i), aes(long,lat)) + labs(x = "longitude", y = "latitude") + 
+    geom_path(data = subset(d, id.tag.year ==  i), aes(long,lat)) + labs(x = "longitude", y = "latitude") + 
     theme(legend.title = element_blank()) +
     ggtitle(paste(i)) + theme(plot.title = element_text(hjust = 0.5)) 
   
   #distance
   plot4 <- 
-    ggplot(aes(timestamp, dist), data = subset(d, id.yr ==  i))  + 
+    ggplot(aes(timestamp, dist), data = subset(d, id.tag.year ==  i))  + 
     theme_bw() + geom_line() + labs(x = "date", y = "distance (degrees)") + 
     scale_x_date(date_breaks = "1 month", date_labels = "%b", date_minor_breaks = "1 month")
   
