ffe4c47c73198f2d0a98230e7896e0455c876bce
evan.buechley <ebuechley@gmail.com>
2020-11-12 12:52:05 -0700

    Rev1 - Figures update

--- 3.Figures.R
+++ 3.Figures.R
@@ -5,236 +5,104 @@ library("gridExtra")
 library(lubridate)
 library(ggmap)
 library(data.table)
+library(ggpubr)
+library(ggnewscale)
+library(plyr)
 
 #Clear workspace
 rm(list = ls())
 
 #set wd
-setwd("~/Google Drive/Research Projects/EV-TV Survival Study/Dataset/Final/Rev1/")
+setwd("~/Google Drive/GitHub/EV.TV.Survival.Study/")
 
 #read data
-d = read.csv("Data/ev.survival.prepared.Rev1.csv")
-d.summ= read.csv("Summary/ev.summary.final.Rev1.csv")
-head(d.summ$start.date)
-d.summ$start.date = md_hm(d.summ$start.date)
-names(d.summ)
+d = read.csv("ev.final.Rev1.survival.prepared.csv", stringsAsFactors=TRUE)
+d.summ= read.csv("ev.summary.final.Rev1.survival.prepared.csv", stringsAsFactors=TRUE)
+d.summ$start.date = ymd_hms(d.summ$start.date)
+d.summ$end.date = ymd(d.summ$end.date)
+summary(d.summ$end.date)
+summary(d.summ$start.date)
+d.summ$mortality.date = ymd_hms(d.summ$mortality.date)
 
-#summary stats for study
-library(plyr)
-names(d.summ)
-nrow(d.summ)
-ddply(d.summ,.(study.name),summarise, n.tagged = length(unique(id))) #n tagged by study
+#check summary
 summary(d.summ)
-unique(d.summ$study.name)
-d.summ$fate = as.factor(d.summ$fate)
-summary(d.summ$fate)
-#summary(d.summ$fate.binned)
-d.summ$population = as.factor(d.summ$population)
-summary(d.summ$population)
-d.summ$sex = as.factor(d.summ$sex)
-summary(d.summ$sex)
 
-#convert to data.table to summarize
-d.dt = setDT(d)
-summary(d.dt)
-
-#check data
-head(d.dt)
-summary(d.dt)
-names(d.dt)
-
-#summarize
-#mort = d.dt[,.(tail(lat,1), tail(long,1)),by = .(id)]
-#summary(mort)
-
-#add headers
-#names(mort) = c("id", "lat.mort", "long.mort")
-
-#add column with mortality location
-#head(d.summ)
-#d.summ$lat.mort = NA
-#d.summ$long.mort = NA
-#summary(mort)
-
-#for (i in unique(mort$id)) { 
-#  d.summ$lat.mort[which(d.summ$id == i)] = mort$lat.mort[which(mort$id == i)]
-#}
-
-#for (i in unique(mort$id)) { 
-#  d.summ$long.mort[which(d.summ$id == i)] = mort$long.mort[which(mort$id == i)]
-#}
-
-#summary(d.summ)
-
-#write.csv(d.summ, "Final Cleaned Data/ev.summary.final.csv", row.names = F)
-
-###################################################################
-#Figure
-###################################################################
-#d.summ = read.csv("Final Cleaned Data/ev.summary.final.2.csv")
-summary(d.summ$fate)
-#d.summ$fate.binned <- factor(d.summ$fate.b, levels = c("alive","confirmed dead","likely dead","confirmed transmitter failure","likely transmitter failure","unknown"))
-#d.summ = d.summ[!(d.summ$fate.final.binned== "alive"),]
-#d.summ$fate.final.binned = as.character(d.summ$fate.final.binned)
-#d.summ$age.simple[which(d.summ$age.in.months < 18)] = "juvenile"
-#d.summ$age.simple[which(d.summ$age.in.months >= 18)] = "adult"
-
-#register_google(key = "xxxx")
-#overview.map = get_map(location = c(20,25), maptype = "terrain", source = "google", zoom = 4)
-#tiff("ev.overview.simple.tiff", units="cm", width=18, height=11, res=300)
-#ggmap(overview.map) + coord_quickmap() + theme_bw() +
-#it = subset(d, d$population == "italy")
-#summary(it$id.tag)
-#it.apollo = subset(it, it$id.tag == "Apollo_16093")
-#it.tobia = subset(it, it$id.tag == "Tobia_48500501945")
-
-#it.summ = subset(d.summ, d.summ$population == "italy")
-#head(it)
-#head(it.summ)
-#ggplot() + annotation_map(map_data("world"), fill = 'grey', color = "white") + coord_quickmap() + theme_bw() +
-#  geom_path(data = it.tobia, aes(long,lat, group = id.tag), alpha = .5, show.legend = FALSE) + 
-#  geom_point(data = it.summ, aes(long.mort, lat.mort, color = fate.final.binned),  size = 3) +
-#  #scale_color_manual(values=c('#e41a1c',"#4daf4a",'#ffff33'), name = "fate") + 
-#  scale_color_viridis_d(begin = .4, direction = -1, name = "fate") + 
-#  #ggtitle("Egyptian vulture") + 
-#  theme(plot.title = element_text(hjust = 0.5)) + labs(x = "longitude", y = "latitude") 
-#dev.off()
-
-####################################################
-# fate summary barplot  
-####################################################
-library(data.table)
-library(ggpubr)
-library(ggnewscale)
-
-#remove alive
-head(d.summ)
-summary(d.summ$fate)
-d.summ2<-d.summ[!(d.summ$fate == "alive"),]
-summary(d.summ2$fate)
-summary(d.summ2$population)
-#d = read.csv("Final Cleaned Data/ev.survival.prepared.csv")
-d$population = as.character(d$population)
-
-#map
-p.map = ggplot() + annotation_map(map_data("world"), fill = 'light grey', color = "white") + coord_quickmap() + theme_bw() +
-  geom_path(data = d, aes(long,lat, group = id), alpha = .5, color = "black") +
-  #scale_color_viridis_d(begin = .2, end = .8, direction = 1, name = "subpopulation:") +
-  #new_scale_color() + 
-  geom_point(data = d.summ2, aes(end.long, end.lat, color = fate),  size = 3) +
-  #scale_color_manual(values=c('#e41a1c',"#4daf4a",'#ffff33', "pink"), name = "fate") + 
-  scale_color_viridis_d(option = "D", begin = 0.4, direction = -1, name = "fate:") + 
-  theme(plot.title = element_text(hjust = 0.5)) + labs(x = "longitude", y = "latitude") 
-+
-  theme(legend.position = "none") 
-p.map
-
-#bar plot 
-# fate by population
 #add population.binned
-summary(d.summ$population)
-d.summ$population.binned = NA
-d.summ$population.binned[d.summ$population == "middle east"] <-  "Middle East"
-d.summ$population.binned[d.summ$population == "caucasus"] <-  "Middle East"
-d.summ$population.binned[d.summ$population == "western europe"] <-  "Western Europe"
-d.summ$population.binned[d.summ$population == "italy"] <-  "Balkans & Italy"
-d.summ$population.binned[d.summ$population == "balkans"] <-  "Balkans & Italy"
-d.summ$population.binned = as.factor(d.summ$population.binned)
-summary(d.summ$population.binned)
-names(d.summ)
-summary(d.summ)
-d.summ = d.summ[complete.cases(d.summ[ , "population.binned"]),]
-#add fate.binned
-summary(d.summ$fate)
-d.summ$fate.binned = NA
-d.summ$fate.binned[d.summ$fate == "alive"] <-  "alive"
-d.summ$fate.binned[d.summ$fate == "confirmed dead"] <-  "dead"
-d.summ$fate.binned[d.summ$fate == "confirmed transmitter failure"] <-  "transmitter failure"
-d.summ$fate.binned[d.summ$fate == "likely dead"] <-  "dead"
-d.summ$fate.binned[d.summ$fate == "likely transmitter failure"] <-  "transmitter failure"
-d.summ$fate.binned[d.summ$fate == "returned to captivity"] <-  NA
-d.summ$fate.binned[d.summ$fate == "unknown"] <-  "unknown"
-d.summ$fate.binned = as.factor(d.summ$fate.binned)
-summary(d.summ$fate.binned)
-names(d.summ)
-d.summ = d.summ[complete.cases(d.summ[ , "fate.binned"]),]
-summary(d.summ$fate.binned)
-
-#map with tracks colored by pop
-names(d)
 unique(d$population)
-d$population[d$population == "middle east"] <-  "Middle East"
-d$population[d$population == "caucasus"] <-  "Middle East"
-d$population[d$population == "western europe"] <-  "Western Europe"
-d$population[d$population == "italy"] <-  "Balkans & Italy"
-d$population[d$population == "balkans"] <-  "Balkans & Italy"
-unique(d$population)
-d$color = d$population
+d$population.binned = NA
+d$population.binned[d$population == "middle east"] <-  "Middle East"
+d$population.binned[d$population == "caucasus"] <-  "Middle East"
+d$population.binned[d$population == "western europe"] <-  "Western Europe"
+d$population.binned[d$population == "italy"] <-  "Balkans & Italy"
+d$population.binned[d$population == "balkans"] <-  "Balkans & Italy"
+unique(d$population.binned)
+d$color = d$population.binned
 d$color[d$color == "Balkans & Italy"] <-  "#800000"
 d$color[d$color == "Western Europe"] <-  "#8B4513"
 d$color[d$color == "Middle East"] <-  "#F4A460"
 head(d)
 head(d.summ)
-d.summ2<-d.summ[!(d.summ$fate == "alive"),]
-p.map = ggplot() + annotation_map(map_data("world"), fill = 'light grey', color = "white") + coord_quickmap() + theme_bw() +
-  geom_path(data = d, aes(long,lat, group = id), color = d$color, alpha = .5) +
+
+#remove alive for plotting end fates
+d.summ2<-d.summ[!(d.summ$fate == "alive"),] 
+d.summ2<-d.summ2[!(d.summ2$population == "unknown"),] 
+
+#rename origin variables
+d.summ2$origin = as.character(d.summ2$origin)
+d.summ2$origin[d.summ2$origin == "N"] <-  "wild"
+d.summ2$origin[d.summ2$origin == "Y"] <-  "captive-raised"
+
+#plot
+setwd("~/Google Drive/Research Projects/EV-TV Survival Study/Figures/")
+
+pmap = ggplot() + annotation_map(map_data("world"), fill = 'light grey', color = "white") + coord_quickmap() + theme_bw() +
+  geom_point(data = d, aes(long,lat, group = id), color = d$color, alpha = .5) +
   scale_color_manual(values= c( "#800000","#F4A460","#8B4513"), name = "subpopulation") + 
   #scale_color_viridis_d(begin = .2, end = .8, direction = 1, name = "subpopulation:") +
   new_scale_color() + 
   geom_point(data = d.summ2, aes(end.long, end.lat, color = fate.binned),  size = 3) +
   scale_color_viridis_d(option = "D", begin = 0.4, direction = -1, name = "fate:") + 
   theme(plot.title = element_text(hjust = 0.5)) + labs(x = "longitude", y = "latitude") 
-+
-  theme(legend.position = "none") 
-p.map
+pmap
 
 #geom_bar(aes(y = (..count..)/sum(..count..)), position = position_dodge()) + scale_y_continuous(labels=scales::percent_format(accuracy=1)) + 
-p1 = ggplot(d.summ, aes(population.binned, fill = fate.binned)) + theme_bw() + 
+p1 = ggplot(d.summ2, aes(population.binned, fill = fate.binned)) + theme_bw() + 
   geom_bar(position = position_dodge()) + ylab("# fates") + xlab("subpopulation") + 
   scale_fill_viridis_d(begin = .4,  direction = -1,  name = "fate:") + theme(legend.position = "none")
-p1 
+  p1 
 
 # fate by age
-names(d.summ)
-p2 = ggplot(d.summ, aes(age.at.fate.simple, fill = fate.binned)) + theme_bw() + 
+p2 = ggplot(d.summ2, aes(age.at.fate.simple, fill = fate.binned)) + theme_bw() + 
   geom_bar(position = position_dodge()) + ylab("# fates") + xlab("age") + 
-  scale_fill_viridis_d(begin = 0.4, direction = -1,  name = "fate:") 
-+ theme(legend.position = "none")
+  scale_fill_viridis_d(begin = 0.4, direction = -1,  name = "fate:") + theme(legend.position = "none")
 p2
 
 # fate by annual stage
-p3 = ggplot(d.summ, aes(fate.annual.stage, fill = fate.final.binned)) + theme_bw() + 
-  geom_bar(position = position_dodge()) + ylab("# fates") + xlab("annual stage") + 
-  scale_fill_viridis_d(begin = 0.4, direction = -1,  name = "fate:") + theme(legend.position = "none")
-p3
+#write a simple file for summarizing the fate.at.annual.stage
+d.summ2$fate.at.annual.stage = NA
+write.csv(d.summ2,"./fate.at.annual.stage.csv", row.names=FALSE)
 
-# fate by captive
-names(d)
-d.summ$origin = d.summ$captive.raised
-p4 = ggplot(d.summ, aes(origin, fill = fate.binned)) + theme_bw() + 
+#p3 = ggplot(d.summ2, aes(fate.annual.stage, fill = fate.final.binned)) + theme_bw() + 
+#  geom_bar(position = position_dodge()) + ylab("# fates") + xlab("annual stage") + 
+#  scale_fill_viridis_d(begin = 0.4, direction = -1,  name = "fate:") + theme(legend.position = "none")
+#p3
+
+# fate by origin
+p4 = ggplot(d.summ2, aes(origin, fill = fate.binned)) + theme_bw() + 
   geom_bar(position = position_dodge()) + ylab("# fates") + xlab("origin") + 
-  scale_fill_viridis_d(begin = .4, direction = -1,  name = "fate:") 
-+ theme(legend.position = "none")
+  scale_fill_viridis_d(begin = .4, direction = -1,  name = "fate:") + theme(legend.position = "none")
 p4
 
-# boxplot fate by end latitude
-#ggplot(d, aes(fate.binned, end.lat)) + 
-#  theme_bw() + geom_boxplot() + scale_fill_viridis_d(begin = .4, direction = -1) 
-
-# boxplot fate by end longitude
-#ggplot(d, aes(fate.binned, end.long)) + theme_bw() + geom_boxplot()
-
 #combine plots
-?ggarrange
-p5 = ggarrange(p.map, 
-               ggarrange(p1, p2, p4, p3, ncol =1, nrow = 4), 
+p5 = ggarrange(pmap, 
+               ggarrange(p1, p2, p4,  ncol =1, nrow = 4), #need to input p3 when ready
                widths = c(3,1.2), legend = "bottom")
 p5
 
 #print
-#tiff("fate.summary.plot.5.tiff", units="cm", width=28, height=15, res=300)
+tiff("fate.summary.plot.Rev1.tiff", units="cm", width=28, height=15, res=300)
 p5
-#dev.off()
+dev.off()
 
 #############################################################################
 # summary stats on data summary
